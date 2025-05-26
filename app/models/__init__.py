5694a676c7087e3c9e8c9f4b82adf19a98d7074f
sskywwalkerr <radmir_rysaev1@mail.ru>
2025-05-26 13:58:14 +0500

    init

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .history_models import SearchHistory
+from .weather_models import CurrentWeather, HourlyForecast
+from .User import User
+
+__all__ = ["SearchHistory", "CurrentWeather", "HourlyForecast", "User"]
