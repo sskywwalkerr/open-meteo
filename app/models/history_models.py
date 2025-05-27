03a7453b3a4b38561f1e7225f60f7d52562aef9e
sskywwalkerr <radmir_rysaev1@mail.ru>
2025-05-27 21:26:17 +0500

    модель для истории

--- app/models/history_models.py
+++ app/models/history_models.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.database.db import Base
 
 
 class SearchHistory(Base):
     __tablename__ = "search_history"
 
-    id = Column(Integer, primary_key=True)
-    city = Column(String(100), nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    city = Column(String, index=True)
     search_time = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="search_history")
